9d730140d4d06c1c8b9c1a1a6c870b316da9ae75
sourav Anil <umeshgupta78361@gmail.com>
2022-12-06 19:09:15 +0530

    I made changes

--- prime.js
+++ prime.js
@@ -1,8 +1,13 @@
-let count =1;
+let count = 0;
 let num = 13;
 for(let i=2; i<num; i++){
-    if(num%2==0){
+    if(num%i==0){
         count++;
     }
 }
-console.log(num-" Is prime");
+if(count==0){
+	console.log(num+" is a prime");
+}
+else{
+	console.log(num+" is not a prime");
+}
